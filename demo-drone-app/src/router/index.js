55283e239f8b6e34ac4efbdc17d6015315ad3002
laddjones <laddjonesiv@gmail.com>
2018-02-12 10:44:28 -0500

    changed file name

--- demo-drone-app/src/router/index.js
+++ demo-drone-app/src/router/index.js
@@ -7,7 +7,7 @@ const routerOptions = [
   { path: '/newmission', component: 'NewMissionPage' },
   { path: '/settings', component: 'Settings'},
   { path: '/homepage', component: 'HomePage'},
-  { path: '/connectdrones', component: 'ConnectDrones'},
+  { path: '/drones', component: 'Drones'},
   { path: '/profile', component: 'Profile'}
 
 ]
