e003039da89242c4d7f2fa8e053a6e8e3f748792
Bruce Crane <samcrane8@gmail.com>
2018-02-18 12:01:37 -0500

    licenses card

--- demo-drone-app/src/components/Settings.vue
+++ demo-drone-app/src/components/Settings.vue
@@ -28,7 +28,7 @@
 					</v-card-text>
 				</v-card>
 
-				<v-card style="margin:20px;">
+				<v-card style="margin:20px;" v-if="false">
 					<v-card-title>
 						<v-flex style="border-bottom-style: solid; border-width: 1px;">
 							<span style="font-size:30px;"> ACCOUNT </span>
@@ -45,24 +45,37 @@
 				<v-layout column style="margin-left:20px;">
 					<v-card style="margin:20px;">
 						<v-card-title>
-							<v-flex style="border-bottom-style: solid; border-width: 1px;">
-								<span style="font-size:30px;"> Personal Details </span>
-							</v-flex>
-							<v-flex>
-								<h3> Address: {{profile_info.address}} </h3>
-								<h3> Date of Birth: {{profile_info.date_of_birth}} </h3>
-								<v-btn outline flat style="color:#253f87;" type="submit">Change Password</v-btn>
-							</v-flex>
+							<v-layout column>
+								<v-flex style="border-bottom-style: solid; border-width: 1px;">
+									<span style="font-size:30px;"> Licenses </span>
+								</v-flex>
+								<v-layout row>
+									<v-flex style="margin-top:12px;">
+										<h3 v-if="profile_info.part_107 != 'null'"> <a :href="profile_info.part_107"> Part 107 </a>
+										</h3>
+										<h3 v-if="profile_info.part_107 == 'null'"> Part 107
+										</h3>
+									</v-flex>
+									<v-flex>
+										<v-btn outline>
+											<span v-if="profile_info.part_107 != 'null'"> EDIT </span>
+											<span v-if="profile_info.part_107 == 'null'"> ADD </span>
+										</v-btn>
+									</v-flex>
+								</v-layout>
+							</v-layout>
 						</v-card-title>
 					</v-card>
-					<v-card style="margin:20px;">
+
+					<v-card style="margin:20px;" v-if="false">
 						<v-card-title>
 							<v-flex style="border-bottom-style: solid; border-width: 1px;">
 								<span style="font-size:30px;"> Preferences </span>
 							</v-flex>
 						</v-card-title>
 					</v-card>
-					<v-card style="margin:20px;">
+
+					<v-card style="margin:20px;" v-if="false">
 						<v-card-text>
 							<v-flex style="border-bottom-style: solid; border-width: 1px;">
 								<span style="font-size:30px;"> Security </span>
@@ -90,7 +103,12 @@ export default {
 				address: '5525 New Wellington Close',
 				username: 'Sam Crane',
 				email: 'samcrane8@gmail.com',
-				image: 'https://avatars0.githubusercontent.com/u/8029035?s=400&v=4'
+				image: 'https://avatars0.githubusercontent.com/u/8029035?s=400&v=4',
+				part_107: 'https://drive.google.com/file/d/1j8jXiXbI05VogHVKivfavdZgbaD0yrwP/view?usp=sharing',
+				documents: [
+					{ type: 'part_107', location: 'https://drive.google.com/file/d/1j8jXiXbI05VogHVKivfavdZgbaD0yrwP/view?usp=sharing'},
+					{ type: 'part_333', location: 'https://drive.google.com/file/d/1j8jXiXbI05VogHVKivfavdZgbaD0yrwP/view?usp=sharing'}
+				]
 			},
 			user_info: {}
 		}
