6ce06037af5eb64956d416da035be4f901e6acd4
adeliyianni3 <adeliyianni5@gatech.edu>
2018-04-09 15:46:25 -0400

    fixing ui for license page

--- demo-drone-app/src/components/settings/SettingsLicenses.vue
+++ demo-drone-app/src/components/settings/SettingsLicenses.vue
@@ -22,10 +22,35 @@
 							</h3>
 						</v-flex>
 						<v-flex class="text-xs-right">
-							<v-btn outline>
-								<span v-if="doc.part_107 != 'null'"> EDIT </span>
-								<span v-if="doc.part_107 == 'null'"> ADD </span>
-							</v-btn>
+							<v-flex xs12 class="text-xs-center text-sm-center text-md-center text-lg-center">
+								<input
+									type="file"
+									style="display: none"
+									id="myBtn"
+									accept="image/*"
+									@change="onFilePicked"
+								>
+								<v-btn outline @click='viewFile(doc)'>
+									<span> EDIT </span>
+								</v-btn>
+								<v-dialog v-model="editFile" max-width="500px">
+								    <v-card>
+								        <v-card-title primary-title>
+								          <div>
+								            <h3 class="headline mb-0">{{currFile}} Upload</h3>
+								            <div>
+								              <img :src="imageUrl" height="150" v-if="imageUrl"/>
+								            </div>
+								          </div>
+								        </v-card-title>
+								        <v-card-actions>
+								          <v-btn color="primary" flat @click='pickFile'>Upload</v-btn>
+								          <v-btn color="primary" flat @click.stop="editFile=false">Save</v-btn>
+								          <v-btn color="primary" flat @click.stop="editFile=false">Cancel</v-btn>
+								        </v-card-actions>
+								    </v-card>
+								</v-dialog>
+							</v-flex>
 						</v-flex>
 					</v-layout>
 				</v-layout>
@@ -58,10 +83,42 @@ export default {
 					{ type: 'Part 107', location: 'https://drive.google.com/file/d/1j8jXiXbI05VogHVKivfavdZgbaD0yrwP/view?usp=sharing'},
 					{ type: 'Part 333', location: 'https://drive.google.com/file/d/1j8jXiXbI05VogHVKivfavdZgbaD0yrwP/view?usp=sharing'}
 				]
-			}
+			},
+			imageUrl: '',
+			imageName: '',
+    		imageFile: '',
+    		currFile: null,
+    		editFile: false
 		}
 	},
 	methods: {
+		pickFile () {
+        	document.getElementById("myBtn").click();
+        },
+        viewFile(a) {
+        	this.currFile = a.type;
+        	this.imageUrl = a.location;
+        	this.editFile=true;
+        },
+      	onFilePicked (e) {
+        	const files = e.target.files
+        	if(files[0] !== undefined) {
+          	this.imageName = files[0].name
+          	if(this.imageName.lastIndexOf('.') <= 0) {
+            	return
+          	}
+          	const fr = new FileReader ()
+          	fr.readAsDataURL(files[0])
+          	fr.addEventListener('load', () => {
+            	this.imageUrl = fr.result
+            	this.imageFile = files[0] // this is an image file that can be sent to server...
+          	})
+        	} else {
+          	this.imageName = ''
+          	this.imageFile = ''
+          	this.imageUrl = ''
+        	}
+      	}
 	},
 	mounted() {
 	}
