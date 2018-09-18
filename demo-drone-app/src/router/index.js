56f611be3af2d5a595260c65ef747d2dc1850ff7
Sam Crane <samcrane8@gmail.com>
2018-09-18 17:45:29 -0400

    vuex data / mustBeLoggedOut guard
    - having the users data in the vuex will help a ton with restricting access. Also will mean I have to check which user it is way less often.
    - now if you're logged in certain pages will be inaccessable.
    - This means no more weird edge cases where the toolbar is wonky... you don't know how long I've waited for this!!!

--- demo-drone-app/src/router/index.js
+++ demo-drone-app/src/router/index.js
@@ -1,9 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import API from '@/mixins/API'
+
 
 const routerOptions = [
-  { path: '/', view: 'LandingPage' },
-  { path: '/login', view: 'LoginPage'},
+  { path: '/', view: 'LandingPage',
+    meta: { mustBeLoggedOut: true} },
+  { path: '/login', view: 'LoginPage',
+    meta: { mustBeLoggedOut: true} },
   { path: '/map', view: 'FlightDetailsPage' },
   { path: '/newflight', view: 'NewFlightPage' },
   { path: '/settings', view: 'SettingsPage'},
@@ -16,7 +22,8 @@ const routerOptions = [
 const routes = routerOptions.map(route => {
   return {
     path: route.path,
-    component: () => import(`@/views/${route.view}/${route.view}.vue`)
+    component: () => import(`@/views/${route.view}/${route.view}.vue`),
+    meta: route.meta
   }
 })
 
@@ -25,6 +32,22 @@ Vue.use(Router)
 const router = new Router({
   mode: 'history',
   routes
+})
+
+router.beforeEach(async (to, from, next) => {
+  if(to.meta.mustBeLoggedOut) {
+    const access_token = window.localStorage.getItem('access_token')
+    var api_data = API.data()
+    var url = api_data.base_url + '/user/is_logged_in/'
+    const response = await axios.get(url, {
+      headers: {'Authorization': 'Bearer ' + access_token}
+    });
+    if (JSON.stringify(response.data) == 'true') {
+      next('/homepage')
+    }
+  }
+
+  next()
 })
 
 export default router;
